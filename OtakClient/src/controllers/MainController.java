faa38eb81ac0c3dd712d34b6d332bdeb5ff3b8e9
Zeshan Aslam <zeshan12aslam@gmail.com>
2016-03-07 22:53:22 -0500

    Created next layout and notifications
    - Created notifications that appear. Uses stacks and queues
    - Created next layout for entering IP
    - Testing communication with webview and java
    - Using maven now

--- OtakClient/src/controllers/MainController.java
+++ OtakClient/src/controllers/MainController.java
@@ -3,6 +3,18 @@ package controllers;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.events.Event;
+import org.w3c.dom.events.EventListener;
+import org.w3c.dom.events.EventTarget;
+import org.w3c.dom.html.HTMLInputElement;
+
+import com.google.common.net.InetAddresses;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Worker.State;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.AnchorPane;
@@ -25,6 +37,34 @@ public class MainController implements Initializable {
 
 		// Make web view responsive
 		new ResponsiveWeb(anchorPane, webView).makeResponsive();
+
+		webView.getEngine().getLoadWorker().stateProperty().addListener(new ChangeListener<State>() {
+			@Override 
+			public void changed(ObservableValue observableValue, State oldState, State newState) {
+				if (newState == State.SUCCEEDED) {
+					Document doc = webView.getEngine().getDocument();
+					Element button = doc.getElementById("btn_ip");
+					((EventTarget) button).addEventListener("click", new EventListener() {
+
+						@Override
+						public void handleEvent(Event event) {
+							HTMLInputElement input = (HTMLInputElement) doc.getElementById("input_ip");
+
+							if (input.getValue() == null || !InetAddresses.isInetAddress(input.getValue()) && !input.getValue().contains("localhost")) {
+								webView.getEngine().executeScript("addNotification('invalid-ip');");
+								return;
+							}
+
+							webView.getEngine().executeScript("addNotification('loading');");
+
+							// Add logic to check if able to connect
+							String ipInput = input.getValue();
+							webView.getEngine().executeScript("addNotification('connected');");
+						}
+					}, false);
+				}
+			}
+		});
 	}
 }
 
